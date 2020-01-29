a99e38710758574c7db8631a0b23c1761911e4ff
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2020-01-29 15:27:36 -0500

    Enable sudo access

--- start.sh
+++ start.sh
@@ -5,4 +5,12 @@
 welcome 'Initializing Sorceress 🧙‍♀️'
 . lib/spells/core/install_prerequisites.sh
 
+announce 'Enabling sudo access'
+sudo echo 'sudo enabled' > /dev/null
+if (( $? == 0 )); then
+  echo "Sudo enabled ✅"
+else
+  abort 'Sudo access is requested to allow scripts to use sudo as necessary.'
+fi
+
 ruby lib/sorceress/boot.rb

efc4fe2a9c93a6f00c8369174bebe7b95c8662f0
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2020-01-31 16:34:54 -0500

    Update chruby installation

--- lib/spells/install/chruby.sh
+++ lib/spells/install/chruby.sh
@@ -6,4 +6,3 @@ brew_install chruby ruby-install
 
 run_command "echo 'source /usr/local/opt/chruby/share/chruby/chruby.sh' >> ~/.bash_profile"
 run_command "echo 'source /usr/local/opt/chruby/share/chruby/auto.sh' >> ~/.bash_profile"
-run_command . ~/.bash_profile
--- lib/spells/install/ruby/chruby.sh
+++ lib/spells/install/ruby/chruby.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 IFS=$'\n\t'
 
 version="${1:--latest}"
+
+run_command "source ~/.bash_profile"
 run_command "ruby-install --no-reinstall ruby $version"
-run_command "chruby $version -- gem install bundler"
+run_command "chruby-exec $version -- gem install bundler"

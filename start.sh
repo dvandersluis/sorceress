7a0da79e1903356f2bf2495ee7a2235fecf856e7
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2020-01-31 00:44:32 -0500

    Shellcheck start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,9 @@ set -o pipefail
     ruby lib/sorceress/boot.rb
 ) > >(tee /tmp/sorceress.log) 2>&1
 
-if [ ${PIPESTATUS[0]} -eq 0 ]; then
+if [ "${PIPESTATUS[0]}" -eq 0 ]; then
   echo
   welcome "Installation complete! ✨"
 fi
 
-printf "Log saved at %s.\n" $(bold "/tmp/sorceress.log")
+printf "Log saved at %s.\n" "$(bold "/tmp/sorceress.log")"
